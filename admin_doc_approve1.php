68343795959b7a5e255b3fa518b36d56e451c484
binibabu443 <binibabu443@gmail.com>
2020-01-21 15:37:27 +0530

    healthcare

---
+++ admin_doc_approve1.php
@@ -0,0 +1,13 @@
+<!-----approve doctor by admin: connection page----->
+
+<?php
+include('connection.php');
+$var = $_GET['id'];
+$sql="UPDATE doctor_registration SET status = 'approved' where doctor_id = '".$var."'";
+    if (mysqli_query($conn,$sql))
+        {
+            header('location:admin_doctors.php');
+        }
+
+mysqli_close($conn);
+?>
---
+++ connection.php
@@ -0,0 +1,12 @@
+<!---connectiion page--->
+
+<?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="health_care";
+	$conn=new mysqli($servername,$username,$password,$dbname);
+	if($conn->connect_error)
+	{
+		die("connection failed".$conn->connect_error);
+	}
